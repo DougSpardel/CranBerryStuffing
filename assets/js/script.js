0545673be1161b9b1b5f131fe58862332ec6f1e2
DougSpardel <douglasspardel@gmail.com>
2023-11-28 20:20:33 -0700

    Merge remote changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,30 +2,42 @@ function showDropdown() {
     const moodInput = document.getElementById("mood-input");
     const playlistDropdown = document.getElementById("playlist-dropdown");
 
-    // Check if the mood input is not empty
     if (moodInput.value.trim() !== "") {
-        // Sample list of 10 songs (you can replace with actual data from your API)
-        const songs = [
-            "Song 1",
-            "Song 2",
-            "Song 3",
-            "Song 4",
-            "Song 5",
-            "Song 6",
-            "Song 7",
-            "Song 8",
-            "Song 9",
-            "Song 10",
-        ];
+        // Make an API request to the Jamendo API to get music suggestions based on mood
+        const apiKey = 'a6645fa6';
+        const baseUrl = 'https://api.jamendo.com/v3.0/';
+        const endpoint = 'tracks/';
+        const params = {
+            client_id: apiKey,
+            format: 'json',
+            limit: 10,
+            mood: moodInput.value, // Use the mood entered by the user
+        };
 
-        // Generate the dropdown list
-        const dropdownHTML = songs.map((song, index) => {
-            return `<div class="dropdown-item">${index + 1}. ${song}</div>`;
-        }).join("");
+        const url = new URL(baseUrl + endpoint);
+        Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
 
-        // Update the playlist dropdown content and make it visible
-        playlistDropdown.innerHTML = dropdownHTML;
-        playlistDropdown.style.display = "block";
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                // Handle the API response and display music suggestions
+                const songs = data.results.map(track => track.name);
+                const dropdownHTML = songs.map((song, index) => {
+                    return `<div class="dropdown-item">${index + 1}. ${song}</div>`;
+                }).join("");
+                playlistDropdown.innerHTML = dropdownHTML;
+                playlistDropdown.style.display = "block";
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                playlistDropdown.innerHTML = "Error fetching suggestions.";
+                playlistDropdown.style.display = "block";
+            });
     } else {
         // Clear and hide the playlist dropdown if the mood input is empty
         playlistDropdown.innerHTML = "";
